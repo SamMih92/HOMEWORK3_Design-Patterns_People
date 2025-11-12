9e250ae74d49c13eabb11332c1e3422afde99c76
SamMih92 <mikhaylov_s@netbro.media>
2025-11-12 16:24:57 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,42 +1,87 @@
-public class PersonBuilder {
-    private String name;
-    private String surname;
-    private int age = Person.UNKNOWN_AGE;
-    private String address;
+import java.util.Objects;
+import java.util.OptionalInt;
 
-    public PersonBuilder setName(String name) {
-        if (name == null) {
-            throw new IllegalArgumentException("Поле имени не может быть пустым");
-        }
-        this.name = name;
-        return this;
-    }
+public class Person {
+    protected static final int UNKNOWN_AGE = -1;
 
-    public PersonBuilder setSurname(String surname) {
-        if (surname == null) {
-            throw new IllegalArgumentException("Поле фамилии не может быть пустым");
-        }
-        this.surname = surname;
-        return this;
-    }
+    protected final String name;
+    protected final String surname;
+    protected int age = UNKNOWN_AGE;
+    protected String address;
 
-    public PersonBuilder setAge(int age) {
-        if (age < Person.UNKNOWN_AGE)
-            throw new IllegalArgumentException("Некорректный возраст");
-        this.age = age;
-        return this;
-    }
-
-    public PersonBuilder setAddress(String address) {
-        this.address = address;
-        return this;
-    }
-
-    public Person build() {
+    public Person(String name, String surname, int age) {
         if (name == null)
-            throw new IllegalStateException("Не указано имя");
+            throw new IllegalArgumentException("Поле не может быть пустым");
         if (surname == null)
-            throw new IllegalStateException("Не указана фамилия");
-        return new Person(name, surname, age);
+            throw new IllegalArgumentException("Поле не может быть пустым");
+        if (age < UNKNOWN_AGE)
+            throw new IllegalArgumentException("Введен некорректный возраст");
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
+
+    public Person(String name, String surname) {
+        if (name == null)
+            throw new IllegalArgumentException("Поле не может быть пустым");
+        if (surname == null)
+            throw new IllegalArgumentException("Поле не может быть пустым");
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public boolean hasAge() {
+        return age != UNKNOWN_AGE;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return hasAge() ? OptionalInt.of(age) : OptionalInt.empty();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) age++;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                (hasAge() ? ", age=" + age : "") +
+                (hasAddress() ? ", address='" + address + '\'' : "") +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(this.surname)
+                .setAddress(this.address)
+                .setAge(0);
+    }
+
 }
